88fd2a46f06c442e3a166bf015f2b8adaf286d0d
bikesh <sitikhubikesh@gmail.com>
2023-02-16 21:55:48 +0545

    authentication part

--- DAO/src/contexts/ProposalContext.js
+++ DAO/src/contexts/ProposalContext.js
@@ -1,42 +1,39 @@
-import React ,{createContext} from "react";
-import { useWeb3React } from "@web3-react/core";
-import Web3 from "web3";
-import { getContract } from "../utils/contract";
-import propsalAbi from "../constant/abi/FCC.json";
+import React, { createContext } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import Web3 from 'web3';
+import { getContract } from '../utils/contract';
+import propsalAbi from '../constant/abi/FCC.json';
 
-
-
-const initialState ={};
+const initialState = {};
 
 export const ProposalContext = createContext(null);
 
-export const ProposalContextProvider = ({children}) => {
+export const ProposalContextProvider = ({ children }) => {
+  const { library, account, chainId } = useWeb3React();
 
-    const {library, account,chainId} = useWeb3React();
+  const addProposal = async (payload) => {
+    const { name, description, amount } = payload;
 
-    const addProposal = async (payload) =>{
-        const {name,description,amount} = payload;
-     
-        const proposal = getContract(library, propsalAbi.abi, "0x5FbDB2315678afecb367f032d93F642f64180aa3");
-        const Funding = await Web3.utils.toWei(amount, "ether");
-        let tx = await proposal.methods.addProposal(name,description,Funding).send({from:account});
-        
+    const proposal = getContract(library, propsalAbi.abi, '0x5FbDB2315678afecb367f032d93F642f64180aa3');
+    const Funding = await Web3.utils.toWei(amount, 'ether');
+    const tx = await proposal.methods.addProposal(name, description, Funding).send({ from: account });
+  };
 
-    }
+  const voteProposal = async (payload) => {
+    const { propsalId, vote } = payload;
+    const proposal = getContract(library, propsalAbi.abi, '0x5FbDB2315678afecb367f032d93F642f64180aa3');
+    const tx = await proposal.methods.voteProposal(propsalId, vote).send({ from: account });
+  };
 
-    const voteProposal = async (payload) =>{
-
-        const {propsalId, vote} = payload;
-        const proposal = getContract(library, propsalAbi.abi, "0x5FbDB2315678afecb367f032d93F642f64180aa3");
-        let tx = await proposal.methods.voteProposal(propsalId, vote).send({from:account});
-    }
-
-    return (
-        <ProposalContext.Provider value={{
-            initialState:initialState,
-            addProposal,
-            voteProposal}}>
-            {children}
-        </ProposalContext.Provider>
-    )
-}
+  return (
+    <ProposalContext.Provider
+      value={{
+        initialState: initialState,
+        addProposal,
+        voteProposal,
+      }}
+    >
+      {children}
+    </ProposalContext.Provider>
+  );
+};
--- DAO/src/contexts/WalletContext.js
+++ DAO/src/contexts/WalletContext.js
@@ -1,22 +1,74 @@
 import PropTypes from 'prop-types';
-import { useEffect, createContext, useCallback, useMemo } from 'react';
+import { useEffect, createContext, useCallback, useMemo, useReducer } from 'react';
 import { useWeb3React } from '@web3-react/core';
 
 import { useWeb3Auth } from '../hooks/useWeb3Auth';
 
 // ----------------------------------------------------------------------
 const noOp = () => {};
-const WalletContext = createContext();
 
 // ----------------------------------------------------------------------
 
-// WalletContextProvider.propTypes = {
-//   children: PropTypes.node,
-// };
+const initialState = {
+  isAuthenticated: false,
+  isInitialized: false,
+  account: null,
+  myRole: null,
+};
+
+const handlers = {
+  INITIALIZE: (state, action) => {
+    const { isAuthenticated, account, myRole } = action.payload;
+    return {
+      ...state,
+      isAuthenticated,
+      isInitialized: true,
+      account,
+      myRole,
+    };
+  },
+  WALLET_LOGIN: (state, action) => {
+    const { account, myRole } = action.payload;
+    return {
+      ...state,
+      isAuthenticated: true,
+      account,
+      myRole,
+    };
+  },
+};
+
+const reducer = (state, action) => (handlers[action.type] ? handlers[action.type](state, action) : state);
+
+const WalletContext = createContext({
+  ...initialState,
+  method: 'wallet',
+  connectWallet: () => Promise.resolve(),
+});
+
+WalletContextProvider.propTypes = {
+  children: PropTypes.node,
+};
 
 function WalletContextProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const { connectMetamask, logOut } = useWeb3Auth();
   const { library, account } = useWeb3React();
+
+  useEffect(() => {
+    const initialize = async () => {
+      dispatch({
+        type: 'INITIALIZE',
+        payload: {
+          isAuthenticated: false,
+          account: null,
+          myRole: null,
+        },
+      });
+    };
+    initialize();
+  }, []);
 
   const slicedWalletAddress = useMemo(
     () => account && `${account?.slice(0, 10)}...${account?.slice(account?.length - 10, account?.length)}`,
@@ -35,18 +87,33 @@ function WalletContextProvider({ children }) {
     [connectMetamask]
   );
 
-  const logout = useCallback((e) => {
+  const logut = useCallback((e) => {
     e.preventDefault();
     localStorage.clear();
     window.location.replace('/');
   }, []);
 
+  const walletLogin = async (account) => {
+    console.log({ account });
+    const role = 'admin';
+    const wallet = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+    dispatch({
+      type: 'WALLET_LOGIN',
+      payload: {
+        account: wallet,
+        myRole: role,
+      },
+    });
+  };
+
   return (
     <WalletContext.Provider
       value={{
+        ...state,
         connectMetamask,
         connectWallet,
         logOut,
+        walletLogin,
         account,
         slicedWalletAddress,
       }}
